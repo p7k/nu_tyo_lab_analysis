763cd835038e8a2768e995451fe0df0708a781ef
Pavel Katsev <pkatsev@gmail.com>
2024-11-14 02:44:13 -0600

    fixed up fold change calcs

--- analysis/util.py
+++ analysis/util.py
@@ -1,3 +1,19 @@
+#
+# graphing and formatting functions
+#
+
 def dedup_trace_legends():
     names = set()
     return lambda trace: trace.update(showlegend=False) if (trace.name in names) else names.add(trace.name)
+
+
+def fmt_strains(strains: dict[str, set[str]]) -> str:
+    return ', '.join([f'{strain_id} ({", ".join(alts)})' for strain_id, alts in strains.items()])
+
+
+#
+# math functions and consts
+#
+
+# based on .55 g•CDW / (au•OD600/L)
+YIELD_OD600_FROM_X_ARG_M = 1.27e4
